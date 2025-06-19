d041715221624800864454f2ab80138649169d7b
mantenimiento matfilm <e3.corest@gmail.com>
2025-06-19 15:58:10 -0500

    refactor: remove unused parameters from isAuthenticatedGuard

--- src/module/auth/guards/is-authenticated.guard.ts
+++ src/module/auth/guards/is-authenticated.guard.ts
@@ -1,11 +1,9 @@
-import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
+import type { NavigationGuardNext } from 'vue-router';
 import { useAuthStore } from '../stores/auth.store';
 import { AuthStatus } from '../interfaces/auth-status.enum';
 
 
 const isAuthenticatedGuard = async (
-  to: RouteLocationNormalized,
-  from: RouteLocationNormalized,
   next: NavigationGuardNext,
 ) => {
   const authStore = useAuthStore();
